8a5f7b0bb7a354187ab08230a061d7c476633de2
GiaMar60 <giamart@libero.it>
2026-08-09 23:01:44 +0200

    Add files via upload

---
+++ photogallery/photo27851/_vti_cnf/sldshow.js
@@ -0,0 +1,7 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|15 Jun 2016 08:23:54 -0000
+vti_extenderversion:SR|5.0.2.2623
+vti_lineageid:SR|{19A5169A-6AA1-4307-AE5B-21A3C9C72B67}
+vti_cacheddtm:TX|15 Jun 2016 08:23:54 -0000
+vti_filesize:IR|4362
+vti_backlinkinfo:VX|-\\ 05\\ -\\ Archivio\\ siti\\ audace\\ \\ (\\ \\ )/Nave\\ Audace\\ D\\ 551/photogallery/photo27851/real.htm
